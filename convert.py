a446e49e46b59f96c13fbfebf9e2e36b93310b1e
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-08-23 15:30:13 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -2,17 +2,17 @@ from PIL import Image
 from pix2tex.cli import LatexOCR
 from sympy.parsing.latex import parse_latex
 
-img = Image.open('C:/Users/Jignas/Downloads/eqn5.jpg')
-model = LatexOCR()
-print(model(img))
-latex_string = model(img)
+if __name__ == '__main__':
+
+    img = Image.open('C:/Users/Jignas/Desktop/yyy.jpg')
+    model = LatexOCR()
+    print(model(img))
+    latex_string = model(img)
 # {\frac{d f}{d t}}=\operatorname*{lim}_{h\to0}{\frac{f(t+h)-f(t)}{h}}
 #latex_string = r" {\frac{d f}{d t}}=\operatorname*{lim}_{h\to0}{\frac{f(t+h)-f(t)}{h}}"
 
-equation = parse_latex(latex_string)
-print (equation)
+    equation = parse_latex(latex_string)
+    print (equation)
 
 # a=sp.Eq(Derivative(f, t), operatorname*(l*(i*m)))
-
-
 
