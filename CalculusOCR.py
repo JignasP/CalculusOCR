951adb9fa94690b2591903b26262c90eb5640dbc
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-08-25 19:09:23 +0530

    Update CalculusOCR.py

--- CalculusOCR.py
+++ CalculusOCR.py
@@ -5,31 +5,39 @@ from sympy.parsing.latex import parse_latex
 from sympy import *
 import sympy
 import re
+import argparse
 
-def ocr(url):
-    img = Image.open(url)
+def ocr(file):
+    img = Image.open(file)
     model = LatexOCR()
     latex_string = model(img)
     equation = parse_latex(latex_string)
-    print(equation)
+    print("Equation : ",equation)
     return equation
 
 def solve(eqn):
-    
+
+
     words = re.findall(r'\b[A-Za-z]+\b', str(eqn))
+
     list = []
     for word in words:
         if word not in dir(sympy) and word not in list:
            list.append(word)
 
     symb = symbols(" ".join(list))
+
+
     ans = eqn.doit()
-    print("After Integration : {}".format(ans))
+    print("Solution : {}".format(ans))
     return ans
 
 if __name__ == '__main__':
 
-    url = '/image.png'
-    eqn = ocr(url)
+    descStr = "CalculusOCR"
+    parser = argparse.ArgumentParser(description=descStr)
+    parser.add_argument('--file', dest='filename', required=True)
+    filename = parser.parse_args().filename
+    eqn = ocr(filename)
     ans = solve(eqn)
 
