140069f7e2013a98b57e02423cf70d23b634e2a6
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-08-27 15:57:27 +0530

    Update CalculusOCR.py

--- CalculusOCR.py
+++ CalculusOCR.py
@@ -1,4 +1,3 @@
-
 from PIL import Image
 from pix2tex.cli import LatexOCR
 from sympy.parsing.latex import parse_latex
@@ -7,37 +6,36 @@ import sympy
 import re
 import argparse
 
-def ocr(file):
+
+def sympy_solve(sympy_equation):
+    words = re.findall(r'\b[A-Za-z]+\b', str(sympy_equation))
+
+    var_list = []
+    for word in words:
+        if word not in dir(sympy) and word not in var_list:
+            var_list.append(word)
+
+    sympy_symbols = symbols(" ".join(var_list))
+
+    solution = sympy_equation.doit()
+
+    return solution
+
+
+def solve(file):
     img = Image.open(file)
     model = LatexOCR()
     latex_string = model(img)
     equation = parse_latex(latex_string)
-    print("Equation : ",equation)
-    return equation
+    return equation, sympy_solve(equation)
 
-def solve(eqn):
-
-
-    words = re.findall(r'\b[A-Za-z]+\b', str(eqn))
-
-    list = []
-    for word in words:
-        if word not in dir(sympy) and word not in list:
-           list.append(word)
-
-    symb = symbols(" ".join(list))
-
-
-    ans = eqn.doit()
-    print("Solution : {}".format(ans))
-    return ans
 
 if __name__ == '__main__':
-
     descStr = "CalculusOCR"
     parser = argparse.ArgumentParser(description=descStr)
     parser.add_argument('--file', dest='filename', required=True)
     filename = parser.parse_args().filename
-    eqn = ocr(filename)
-    ans = solve(eqn)
 
+    eqn, sol = solve(filename)
+    print("Equation : ", eqn)
+    print("Solution : {}".format(sol))
