024beb3a5c5c8d37928bf840770068fc87427015
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-09-02 22:58:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup,find_packages
-
+from setuptools import setup, find_packages
 
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
+
 
 
 setup(
@@ -24,14 +24,14 @@ setup(
         'calculus'
     ],
     packages=find_packages(),
-    install_requires=['pillow>=10.0.0','pix2tex>=0.1.2 ','antlr4-python3-runtime==4.11.0'],
-   
+
+
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Text Processing :: Markup :: LaTeX',        
+        'Topic :: Text Processing :: Markup :: LaTeX',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
     ],
