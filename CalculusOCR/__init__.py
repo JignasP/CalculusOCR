6b131ad8254a6094f1fb911dd4f2a4bcb3e50110
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-09-02 23:00:23 +0530

    Update __init__.py

--- CalculusOCR/__init__.py
+++ CalculusOCR/__init__.py
@@ -1,1 +1,5 @@
+import subprocess
+subprocess.call(['pip', 'install', 'antlr4-python3-runtime==4.11.0'])
+subprocess.call(['pip', 'install', 'pix2tex==0.1.2'])
+
 from .CalculusOCR import solveimage ,sympy_solve
