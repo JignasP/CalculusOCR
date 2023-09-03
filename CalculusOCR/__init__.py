b17274b122a75f7e1654169e42e3f3a735ab3bc1
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-09-03 10:43:15 +0530

    Update __init__.py

--- CalculusOCR/__init__.py
+++ CalculusOCR/__init__.py
@@ -1,5 +1,20 @@
 import subprocess
-subprocess.call(['pip', 'install', 'antlr4-python3-runtime==4.11.0'])
-subprocess.call(['pip', 'install', 'pix2tex==0.1.2'])
 
-from .CalculusOCR import solveimage ,sympy_solve
+
+def check_and_install(package, version=None):
+    result = subprocess.run(['pip', 'show', package], stdout=subprocess.PIPE, text=True)
+    found = version is None or version in result.stdout
+    if not found:
+        subprocess.call(['pip', 'install', f'{package}' + (f'=={version}' if version else '')])
+    return found
+
+
+with open('requirements.txt', 'r') as requirements_file:
+    for line in requirements_file:
+        package_info = line.strip().split('==')
+        if len(package_info) >= 1:
+            package_name = package_info[0]
+            package_version = package_info[1] if len(package_info) == 2 else None
+            check_and_install(package_name, package_version)
+
+from .CalculusOCR import solveimage, sympy_solve
