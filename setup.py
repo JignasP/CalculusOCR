47f5c050c6337c2142ff37dbced5abf3b17eec45
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-09-04 16:57:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 import subprocess
 import os
-
+'''
 cocr_remote_version = (
     subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
     .stdout.decode("utf-8")
@@ -17,7 +17,7 @@ with open('CalculusOCR/version.txt', "w") as file:
     file.write(cocr_remote_version)
     print("Done")
 
-'''
+
 if "-" in cocr_remote_version:
 
     v,i,s = cocr_remote_version.split("-")
@@ -29,17 +29,24 @@ assert os.path.isfile("CalculusOCR/version.py")
 
 with open("CalculusOCR/VERSION", "w", encoding="utf-8") as fh:
     fh.write("%s\n" % cocr_remote_version)
+
+
+
+with open('CalculusOCR/version.txt', "r") as file:
+    version_no = file.read()
+    
 '''
+
+try:
+    git_tag = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"]).strip().decode("utf-8")
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
 
-with open('CalculusOCR/version.txt', "r") as file:
-    version_no = file.read()
 
 setup(
     name='CalculusOCR',
-    version= version_no,
+    version= git_tag,
     description=' CalculusOCR: A Vision Transformer that can perform optical character recognition on handwritten calculus expressions and outputs LaTeX code, Sympy equation and solution.',
     long_description=long_description,
     long_description_content_type='text/markdown',
