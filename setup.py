e506b3373d5bed6c552d6768ec7bd64c0393f217
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-09-04 16:39:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,12 @@ cocr_remote_version = (
 )
 
 print(cocr_remote_version)
+if cocr_remote_version != '':
+    with open('CalculusOCR/version.txt', "w") as file:
+        file.write(cocr_remote_version)
+        print("Done")
 
+'''
 if "-" in cocr_remote_version:
 
     v,i,s = cocr_remote_version.split("-")
@@ -24,16 +29,17 @@ assert os.path.isfile("CalculusOCR/version.py")
 
 with open("CalculusOCR/VERSION", "w", encoding="utf-8") as fh:
     fh.write("%s\n" % cocr_remote_version)
-
+'''
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
 
-
+with open("version.txt", "r") as file:
+    version_no = file.read()
 
 setup(
     name='CalculusOCR',
-    version=str(cocr_remote_version),
+    version= version_no,
     description=' CalculusOCR: A Vision Transformer that can perform optical character recognition on handwritten calculus expressions and outputs LaTeX code, Sympy equation and solution.',
     long_description=long_description,
     long_description_content_type='text/markdown',
